2f37417a9e1df55bf7ef9dbe81e14788c79e43ac
Dor Katzir <katzirdor@gmail.com>
2021-08-08 18:16:27 +0300

    added effects and animations with jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
+    // Adding Sticky Navigation //////
     $(".about-section").waypoint(function(direction){
        if(direction == "down"){
             $("nav").addClass("sticky-nav");
         }
         else{
            $("nav").removeClass("sticky-nav");
-        }
-    //    if(direction == "up"){
-    //         $("nav").removeClass("sticky-nav");
-    //    }
-       
+        }  
     });
+    
+    // Animation on Scroll //////
+    $(".js--about-section").waypoint(function(direction){  
+        $(".js--about-box").addClass("animate__animated animate__fadeIn");   
+    }, {
+        offset: "50%"
+    });
+    
+    $(".js--services-section").waypoint(function(direction){  
+        $(".js--service-box").addClass("animate__animated animate__zoomIn");   
+    }, {
+        offset: "50%"
+    });
+    
+    $(".js--packages-section").waypoint(function(direction){  
+        $(".js--package-box").addClass("animate__animated animate__pulse");   
+    });
+
+
 });
